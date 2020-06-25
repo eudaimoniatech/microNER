e98f4a941df2800cf1d2edd96e6d72694f87a0b1
Philipp Sauter <sauterp@protonmail.com>
2020-06-25 17:03:01 +0200

    exported all models

--- export_models.py
+++ export_models.py
@@ -5,10 +5,6 @@ def export_model(filename):
     n.model.save("exported_models/"+filename)
 
 export_model("conll.h5")
-export_model("conll.indexes")
 export_model("germeval-conll.h5")
-export_model("germeval-conll.indexes")
 export_model("germeval.h5")
-export_model("germeval.indexes")
 export_model("germeval-inner.h5")
-export_model("germeval-inner.indexes")
