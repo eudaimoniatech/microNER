b200952a8aa9a89f6c7d083c269511f0c78e65da
Philipp Sauter <sauterp@protonmail.com>
2020-06-26 21:31:02 +0200

    export and convert models to pb format so they may be used without Keras

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ We provide four pre-trained models for German.
 
 All details: https://uhh-lt.github.io/microNER
 
+# Export as SavedModel
+We managed to export the models with specific versions of python, tensorflow and keras. To install them you need Miniconda3.
+To export this model as a TensorFlow SavedModel run:
+
+	conda env create -f=conversionenv.yml
+	conda activate microNERattempt1
+	git submodule init
+	git submodule update
+	python export_models.py
+	python convert_models.py
+
+The converted models are now in `converted_models`.
--- export_models.py
+++ export_models.py
@@ -1,10 +1,19 @@
+# Since we were unable to reproduce the build of this Dockerfile, we had to export the models from the docker image uhhlt/microner:v0.1 on docker hub.
+import tensorflow as tf
 import scripts.ner as ner
 
 def export_model(filename):
+    folder = "exported_models/"
     n = ner.NerModel(filename)
-    n.model.save("exported_models/"+filename)
+    n.model.save(folder+filename)
+
+    mjson = n.model.to_json()
+    f = open(folder+filename+".json", "w")
+    f.write(mjson)
+    f.close()
 
 export_model("conll.h5")
 export_model("germeval-conll.h5")
 export_model("germeval.h5")
 export_model("germeval-inner.h5")
+
